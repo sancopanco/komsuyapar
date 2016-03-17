5990cabfd9755e10cdad44aae21c85d09015488b
Ali Kargın <ali.sancopanco@gmail.com>
2016-03-18 01:14:45 +0200

    fix things

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -8,11 +8,11 @@ uri = URI.parse(url)
 
 REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password, thread_safe: true)
 
-# sidekiq configurations
-sidekiq_config = { url: url, driver: 'hiredis', namespace: 'komsuyapar' }
-Sidekiq.configure_server do |config|
-  config.redis = sidekiq_config
-end
-Sidekiq.configure_client do |config|
-  config.redis = sidekiq_config
-end
+# # sidekiq configurations
+# sidekiq_config = { url: url, driver: 'hiredis', namespace: 'komsuyapar' }
+# Sidekiq.configure_server do |config|
+#   config.redis = sidekiq_config
+# end
+# Sidekiq.configure_client do |config|
+#   config.redis = sidekiq_config
+# end
