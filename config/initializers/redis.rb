0cd9d53fbe864322ec709a802e4fe53b4d5e20b8
Ali Kargın <ali.sancopanco@gmail.com>
2016-03-18 01:15:07 +0200

    fix things

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 require 'redis-namespace'
-require 'sidekiq/middleware/i18n'
+#require 'sidekiq/middleware/i18n'
 
 url = YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env]
 uri = URI.parse(url)
